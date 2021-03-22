6082f161a50155e796918eb21965d08317e2a737
Chris Wodarczyk <u031583@Global.Bio-Rad.com>
2021-03-22 14:32:33 -0700

    Adding debug converter for WPF

---
+++ Framework/WPF/Core/Framework.WPF.Core/Converters/Debugger/DebugConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Framework.WPF.Core.Converters.Debugger
+{
+	/// <summary>
+	/// Implementation of <see cref="IValueConverter"/> which can be helpful in debugging binding values
+	/// </summary>
+	public class DebugConverter : IValueConverter
+	{
+		/// <inheritdoc/>
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (System.Diagnostics.Debugger.IsAttached)
+				System.Diagnostics.Debugger.Break();
+			return value;
+		}
+
+		/// <inheritdoc/>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (System.Diagnostics.Debugger.IsAttached)
+				System.Diagnostics.Debugger.Break();
+			return value;
+		}
+	}
+}
