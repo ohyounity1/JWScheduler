48554a29c8879b50964f9250e8d8d39be9d1a627
Chris Wodarczyk <u031583@Global.Bio-Rad.com>
2021-03-22 17:52:17 -0700

    Double extensions addition

---
+++ Framework/Core/Framework.Core/Math/DoubleExtensions.cs
@@ -0,0 +1,24 @@
+﻿using SystemMath = System.Math;
+
+namespace Framework.Core.Math
+{
+	/// <summary>
+	/// Some core math extension helpers
+	/// </summary>
+	public static class DoubleExtensions
+	{
+		/// <summary>
+		/// Compares two doubles to a given precision,
+		/// Default is <see cref="double.Epsilon"/>
+		/// </summary>
+		/// <param name="doubleValue">Double on the left</param>
+		/// <param name="compareTo">Double on the right</param>
+		/// <param name="epsilon">How close to compare to</param>
+		/// <returns></returns>
+		public static bool IsClose(this double doubleValue, double compareTo, double epsilon = double.Epsilon)
+		{
+			// Allows left/right side to be less, greater than the other
+			return SystemMath.Abs(doubleValue - compareTo) < epsilon;
+		}
+	}
+}
