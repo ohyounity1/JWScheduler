34e6f6d34304dd2dea04559f0a7d45f791252c81
Chris Wodarczyk <u031583@Global.Bio-Rad.com>
2021-03-24 12:24:03 -0700

    Adding ViewModelLocator pattern to framework core wpf

---
+++ Framework/Core/Framework.Core/Language/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Framework.Core.Language
+{
+	public abstract class Singleton<T>
+	{
+		private static readonly Lazy<T> _instance = new(() => Activator.CreateInstance<T>());
+
+		public static T Instance => _instance.Value;
+	}
+}
---
+++ Framework/WPF/Core/Framework.WPF.Core/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,25 @@
+﻿using Framework.Core.Language;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Framework.WPF.Core.ViewModel
+{
+	public abstract class ViewModelLocator<TViewModel>
+	{
+		protected bool IsDesignTime => DesignerProperties.GetIsInDesignMode(DependencyObjectSingle.Instance);
+
+		protected TViewModel RuntimeViewModel { get; }
+
+		protected TViewModel DesignTimeViewModel { get; }
+
+		protected ViewModelLocator(TViewModel designTimeViewModel)
+		{
+			DesignTimeViewModel = designTimeViewModel;
+			RuntimeViewModel = CommonServiceLocator.ServiceLocator.Current.GetInstance<TViewModel>();
+		}
+
+		private class DependencyObjectSingle : Singleton<DependencyObject>
+		{ 
+		}
+	}
+}
